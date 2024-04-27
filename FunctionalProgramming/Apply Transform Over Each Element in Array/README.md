54b978604ab6a71d29800b94e65959071891091b
Selim Biber <selim_biber@hotmail.com>
2024-04-27 23:57:21 +0300

    Submit the last coding solution

---
+++ FunctionalProgramming/Apply Transform Over Each Element in Array/README.md
@@ -0,0 +1,16 @@
+# Apply Transform Over Each Element in Array
+
+## Question Description
+
+Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+## Solution Languages
+
+- [x] JavaScript
+- [ ] Python
+
+## [Question Source](https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/)
---
+++ FunctionalProgramming/Apply Transform Over Each Element in Array/solution.js
@@ -0,0 +1,28 @@
+const map = function (arr, fn) {
+  const RESULT = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    RESULT.push(fn(arr[i], i));
+  }
+
+  return RESULT;
+};
+
+/*NOTE - Example
+
+Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+Output: [2,3,4]
+Explanation:
+    const newArray = map(arr, plusone); // [2,3,4]
+
+The function increases each value in the array by one. 
+*/
+
+/* NOTE - Info
+
+submitted at Apr 27, 2024 23:54
+
+runtime 50ms and beats 65.15% of users with JavaScript
+
+memory 48.61 MB and beats 69.31% of users with JavaScript
+*/
