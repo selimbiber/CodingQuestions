d2e0fafaed3e4a97dbff1fa6b62aacda88512fa5
Selim Biber <selim_biber@hotmail.com>
2024-06-06 22:26:56 +0300

    Submit the last solution

---
+++ FunctionalProgramming/Student's Final Grade/solution.js
@@ -0,0 +1,20 @@
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if (exam > 75 && projects >= 5) return 90;
+  if (exam > 50 && projects >= 2) return 75;
+  return 0;
+}
+
+/* NOTE Examples
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0
+*/
