3d212c21b8098c5883daf8200e8c73b69ebdc363
Selim Biber <selim_biber@hotmail.com>
2024-06-16 12:56:01 +0300

    Submit solution of the daily coding question

---
+++ StringManipulation/findTheArrayConcVal/solution.js
@@ -0,0 +1,58 @@
+const findTheArrayConcVal = function (nums) {
+  let concatenationVal = 0;
+
+  while (nums.length > 0) {
+    if (nums.length === 1) return (concatenationVal += nums.pop());
+    const firstVal = String(nums.shift());
+    const lastVal = String(nums.pop());
+    concatenationVal += Number(firstVal + lastVal);
+  }
+
+  return concatenationVal;
+};
+
+/* NOTE Examples
+
+Example 1:
+
+Input: nums = [7,52,2,4]
+Output: 596
+Explanation: Before performing any operation, nums is [7,52,2,4] and concatenation value is 0.
+ - In the first operation:
+We pick the first element, 7, and the last element, 4.
+Their concatenation is 74, and we add it to the concatenation value, so it becomes equal to 74.
+Then we delete them from nums, so nums becomes equal to [52,2].
+ - In the second operation:
+We pick the first element, 52, and the last element, 2.
+Their concatenation is 522, and we add it to the concatenation value, so it becomes equal to 596.
+Then we delete them from the nums, so nums becomes empty.
+Since the concatenation value is 596 so the answer is 596.
+
+Example 2:
+
+Input: nums = [5,14,13,8,12]
+Output: 673
+Explanation: Before performing any operation, nums is [5,14,13,8,12] and concatenation value is 0.
+ - In the first operation:
+We pick the first element, 5, and the last element, 12.
+Their concatenation is 512, and we add it to the concatenation value, so it becomes equal to 512.
+Then we delete them from the nums, so nums becomes equal to [14,13,8].
+ - In the second operation:
+We pick the first element, 14, and the last element, 8.
+Their concatenation is 148, and we add it to the concatenation value, so it becomes equal to 660.
+Then we delete them from the nums, so nums becomes equal to [13].
+ - In the third operation:
+nums has only one element, so we pick 13 and add it to the concatenation value, so it becomes equal to 673.
+Then we delete it from nums, so nums become empty.
+Since the concatenation value is 673 so the answer is 673.
+
+*/
+
+/* NOTE - Results
+    
+submitted at Jun 14, 2024 20:14
+
+runtime 48ms and beats 90.21% of users with JavaScript
+
+memory 50.30 MB and beats 88.81% of users with JavaScript
+*/
