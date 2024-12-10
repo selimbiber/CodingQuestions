22cc45c500994ac60ae55fc7e2146095714be74e
Selim Biber <selim_biber@hotmail.com>
2024-12-10 16:43:29 +0300

    Submit solution of the daily coding question

---
+++ DataStructures/Arrays/stableMountains/solution.ts
@@ -0,0 +1,9 @@
+function stableMountains(height: number[], threshold: number): number[] {
+  const stables: number[] = [];
+
+  for (let i = 1; i < height.length; i++) {
+    if (height[i - 1] > threshold) stables.push(i);
+  }
+
+  return stables;
+}
