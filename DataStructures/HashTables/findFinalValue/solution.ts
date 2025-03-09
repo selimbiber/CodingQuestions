1109d1e20147f542e880115c8c83725d28d5a8fc
Selim Biber <selim_biber@hotmail.com>
2025-03-10 00:01:12 +0300

    Submit TypeScript solution for coding question from 2025-03-09

---
+++ DataStructures/HashTables/findFinalValue/solution.ts
@@ -0,0 +1,9 @@
+function findFinalValue(nums: number[], original: number): number {
+  const numSet = new Set(nums);
+
+  while (numSet.has(original)) {
+    original *= 2;
+  }
+
+  return original;
+}
